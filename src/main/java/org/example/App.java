8c5f3a7b4d99a674aa990e457496c70946c5ad4c
SangWon7242 <gotkdqja@gmail.com>
2023-03-23 20:18:26 +0900

    게시물 리스팅, 추가 기능 구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,48 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
 
+    List<Article> articles = new ArrayList<>();
+    int articleLastId = 0;
+
     while (true) {
       System.out.printf("명령어) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("article list")) {
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = ++articleLastId;
+
+        Article article = new Article(id, title, body);
+        articles.add(article);
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      }
+      else if(cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
+
+        if(articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("번호 / 제목");
+
+        for( Article article : articles ) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
       }
       else if(cmd.equals("system exit")) {
         System.out.println("시스템 종료");
@@ -22,5 +53,26 @@ public class App {
       }
     }
     sc.close();
+  }
+}
+
+class Article {
+  public int id;
+  public String title;
+  public String body;
+
+  public Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
+  @Override
+  public String toString() {
+    return "Article{" +
+        "id=" + id +
+        ", title='" + title + '\'' +
+        ", body='" + body + '\'' +
+        '}';
   }
 }
