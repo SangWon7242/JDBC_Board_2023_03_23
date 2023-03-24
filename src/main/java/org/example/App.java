c8d3907ed560ee65ba4b14a5b11e1351ff1829db
SangWon7242 <gotkdqja@gmail.com>
2023-03-24 19:20:47 +0900

    게시물 리스트 데이터를 DB에서 가져오기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,8 +8,6 @@ import java.util.Scanner;
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
-
-    List<Article> articles = new ArrayList<>();
     int articleLastId = 0;
 
     while (true) {
@@ -45,8 +40,6 @@ public class App {
 
           pstmt = conn.prepareStatement(sql);
           int affectedRows = pstmt.executeUpdate();
-
-          System.out.println("affectedRows : " + affectedRows);
         }
         catch(ClassNotFoundException e){
           System.out.println("드라이버 로딩 실패");
@@ -73,13 +66,76 @@ public class App {
           }
         }
 
-        Article article = new Article(id, title, body);
-        articles.add(article);
-
-        System.out.println("생성된 게시물 객체 : " + article);
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
       }
       else if(cmd.equals("/usr/article/list")) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        List<Article> articles = new ArrayList<>();
+
+        try{
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+          String sql = "SELECT *";
+          sql += " FROM article";
+          sql += " ORDER BY id DESC";
+
+          pstmt = conn.prepareStatement(sql);
+          rs = pstmt.executeQuery(sql);
+
+          while (rs.next()) {
+            int id = rs.getInt("id");
+
+            String regDate = rs.getString("regDate");
+            String updateDate = rs.getString("updateDate");
+            String title = rs.getString("title");
+            String body = rs.getString("body");
+
+            Article article = new Article(id, regDate, updateDate, title, body);
+            articles.add(article);
+          }
+
+        }
+        catch(ClassNotFoundException e){
+          System.out.println("드라이버 로딩 실패");
+        }
+        catch(SQLException e){
+          System.out.println("에러: " + e);
+        }
+        finally{
+          try{
+            if( rs != null && !rs.isClosed()){
+              rs.close();
+            }
+          }
+          catch( SQLException e){
+            e.printStackTrace();
+          }
+          try{
+            if( pstmt != null && !pstmt.isClosed()){
+              pstmt.close();
+            }
+          }
+          catch( SQLException e){
+            e.printStackTrace();
+          }
+          try{
+            if( conn != null && !conn.isClosed()){
+              conn.close();
+            }
+          }
+          catch( SQLException e){
+            e.printStackTrace();
+          }
+        }
+
+
         System.out.println("== 게시물 리스트 ==");
 
         if(articles.isEmpty()) {
@@ -108,11 +164,21 @@ public class App {
 
 class Article {
   public int id;
+  public String regDate;
+  public String updateDate;
   public String title;
   public String body;
 
   public Article(int id, String title, String body) {
     this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
+  public Article(int id, String regDate, String updateDate, String title, String body) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
     this.title = title;
     this.body = body;
   }
